7bf3f885cfc071e5def5f9dc1f00012d612cef33
PrachiLife <prachi23january@gmail.com>
2024-09-07 17:40:04 +0530

    initialize the project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import StatForm from "./components/StatForm.vue";
+</script>
+
+<template>
+  <div class="container">
+    <StatForm />
+  </div>
+</template>
---
+++ src/components/StatForm.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="container">
+        Stat the Form
+    </div>
+</template>
